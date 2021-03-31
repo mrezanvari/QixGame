a1deeea24a6f070abc17d623cff080e3cad87772
Natalie Doak <natalieannedoak@gmail.com>
2021-03-30 23:06:26 -0400

    WIP

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import sys, pygame
 pygame.init()
 
-screen = pygame.display.set_mode((400, 400))
-done = False
- 
-while not done:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        done = True
-        
-        pygame.display.flip()
+WIDTH, HEIGHT = 900, 500
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Qix Game")
+
+WHITE = (255, 255, 255)
+ARROW_KEYS = [pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP, pygame.K_DOWN]
+FPS = 60
+
+def draw_window():
+        WIN.fill(WHITE)
+        pygame.display.update()
+
+def main():
+        clock = pygame.time.Clock()
+        run = True
+        while run: 
+                clock.tick(FPS)
+                for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                                run = False
+                draw_window()
+
+        pygame.quit();
+
+if __name__ == "__main__":
+        main()
